90cad677aa584e59976653cc7f813364d8182594
Constanza Mariño <marinoconstanza03@gmail.com>
2025-07-01 20:42:43 -0300

    change

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -3,7 +3,57 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
-    }
+        Console.WriteLine("What is your grade percentage? ");
+        string user_percentage = Console.ReadLine();
+        int percentage = int.Parse(user_percentage);
+
+        string letter_grade = "";
+
+        if (percentage >= 90)
+        {
+            letter_grade = "A";
+        }
+
+        else if (percentage >= 80)
+        {
+            letter_grade = "B";
+        }
+
+        else if (percentage >= 70)
+        {
+            letter_grade = "C";
+        }
+
+        else if (percentage >= 60)
+        {
+            letter_grade = "D";
+
+        }
+
+        else
+        {
+            letter_grade = "F";
+        }
+
+        Console.WriteLine($"Your final grade is {letter_grade}.");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You have passed this course!");
+        }
+
+        else
+        {
+            Console.WriteLine("You haven't passed. Good luck for the next time, you can do it!");
+        }
+
+
+
+
+
+    }  
+    
+   
 }
