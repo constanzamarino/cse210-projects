ddef779e18d8ebf34be21874ed24ee381cc8f651
Constanza Mariño <marinoconstanza03@gmail.com>
2025-07-02 16:01:03 -0300

    change

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Write the magic number in here --> ");
+        int userMagic_number = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Type your guess right here --> ");
+        int user_guess = int.Parse(Console.ReadLine());
+
+        while (user_guess != userMagic_number)
+        {
+
+            if (user_guess > userMagic_number)
+            {
+                Console.WriteLine("Guess lower!");
+            }
+            else if (user_guess < userMagic_number)
+            {
+                Console.WriteLine("Guess higher!");
+            }
+            user_guess= int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Congrats! You have guessed it!");
+
+
     }
 }
+    
+
+
+
+        
