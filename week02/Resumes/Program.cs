c4d6a06677fdf2628863cc2b5828d8ed27ea3830
Constanza Mariño <marinoconstanza03@gmail.com>
2025-07-08 19:42:37 -0300

    Change in Resumes

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Senior Engineering Manager";
+        job1._company = "Samsung";
+        job1._startYear = 2015;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Production Supervisor";
+        job2._company = "Coca-Cola Company";
+        job2._startYear = 2021;
+        job2._endYear = 2024;
+
+        Resume resume = new Resume();
+        resume._name = "Elizabeth Sallow";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
+
+
+
     }
 }
