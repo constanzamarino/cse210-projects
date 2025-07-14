0af77f7566240cf3c52c498cc96266ea6d050c2d
Constanza Mariño <marinoconstanza03@gmail.com>
2025-07-14 16:58:12 -0300

    Added new code int.TryParse

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -7,47 +7,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to MyJournal Program!");
-        Console.WriteLine("Select one of the options presented below:");
-        Console.WriteLine("1. Write a new entry");
-        Console.WriteLine("2. Display journal");
-        Console.WriteLine("3. Load journal ");
-        Console.WriteLine("4. Save journal");
-        Console.WriteLine("5. Quit");
-       
+        
+
         Journal journal = new Journal();
+
         int user_choice = 0;
 
+
         while (user_choice != 5)
-
-         Console.WriteLine("Enter your choice:");
-         string user_input = Console.ReadLine();
-         user_choice = int.Parse(user_input);
-
-        if (user_choice == 1)
         {
-            journal.AddEntry();
-        }
+            Console.WriteLine("Welcome to MyJournal Program!");
+            Console.WriteLine("Select one of the options presented below:");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display journal");
+            Console.WriteLine("3. Load journal ");
+            Console.WriteLine("4. Save journal");
+            Console.WriteLine("5. Quit");
 
-        else if (user_choice == 2)
-        {
-            journal.DisplayJournal();
-        }
+            Console.WriteLine("Enter your choice:");
 
-        else if (user_choice == 3)
-        {
-            journal.LoadFromFile();
-        }
+            string user_input = Console.ReadLine();
 
-        else if (user_choice == 4)
-        {
-            journal.SaveToFile();
+            if (!int.TryParse(user_input, out user_choice))
+            {
+                Console.WriteLine("Invalid input. Please enter a number from 1 to 5.");
+                continue;
+            }
 
-        }
+            if (user_choice == 1)
+            {
+                journal.AddEntry();
+            }
 
-        else
-        {
-            Console.WriteLine("I don't understand that answer.");
-        }
-    }
+            else if (user_choice == 2)
+            {
+                journal.DisplayJournal();
+            }
+
+            else if (user_choice == 3)
+            {
+                journal.LoadFromFile();
+            }
+
+            else if (user_choice == 4)
+            {
+                journal.SaveToFile();
+
+            }
+
+            else if (user_choice == 5)
+            {
+                Console.WriteLine("Closing MyJournal...");
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid option. Please select (1-5).");
+            }
+
+    }   }
 }
