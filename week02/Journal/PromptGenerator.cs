32a7eca3b87754c079b3e983d0c18691db68cb2e
Constanza Mariño <marinoconstanza03@gmail.com>
2025-07-14 17:00:10 -0300

    Logic fixed

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -5,23 +5,28 @@ using System.Collections.Generic;
 
 public class PromptGenerator
 {
-    public List<string> _prompts = new List<string>();
+    public List<string> _prompts;
 
+
+    public PromptGenerator()
+
+    {
+        _prompts = new List<string>
+        {
+            "How I have been feeling lately?",
+            "What was the most exciting thing that happened to me today?",
+            "Did I learn something new today, whether a skill or a topic?",
+            "What is a limiting belief I need to let go of?",
+            "What song, movie or book has resonated with me recently?"
+
+        };
+
+    }
     public string GetRandomPrompt()
     {
         Random _randomNumberGenerator = new Random();
         int index = _randomNumberGenerator.Next(_prompts.Count);
         return _prompts[index];
-    }
-    public void LoadRandomPrompt()
-
-    {
-        _prompts.Add("How I have been feeling lately?");
-        _prompts.Add("What was the most exciting thing that happened to me today?");
-        _prompts.Add("Did I learn something new today, whether a skill or a topic?");
-        _prompts.Add("What is a limiting belief I need to let go of?");
-        _prompts.Add("What song, movie or book has resonated with me recently?");
-
     }
 
 }
