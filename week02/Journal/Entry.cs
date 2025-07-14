55df675b066eee2c12d088022b2e8dcc9060c314
Constanza Mariño <marinoconstanza03@gmail.com>
2025-07-13 21:57:04 -0300

    _userName added

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -2,14 +2,19 @@ using System;
 
 public class Entry
 {
+    
     public string _date;
+
+    public string _userName;
 
     public string _randomPrompt;
 
     public string _entryText;
 
+  
+
     public void Display()
     {
-        Console.WriteLine($"{_date}\n{_randomPrompt}\n{_entryText}\n");
+        Console.WriteLine($"By: {_userName}\n{_date}\n{_randomPrompt}\n{_entryText}\n");
     }
 }
