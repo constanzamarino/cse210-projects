b75c41906380290c33eae0fad85be80c7e206dd6
Constanza Mariño <marinoconstanza03@gmail.com>
2025-07-18 19:16:01 -0300

    Starting with the project

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Console.WriteLine("Welcome to the ScriptureMemorizer program!");
+        Console.WriteLine("Press enter to continue. Type 'quit' as per your preference or after you are done. ");
+        string user_choice = Console.ReadLine();
+
+
+
+        while (user_choice != "quit".ToLower())
+        {
+            if (user_choice == "")
+            {
+
+            }
+
+            else if (user_choice == "quit")
+            {
+                Console.WriteLine();
+            }
+        }
     }
 }
