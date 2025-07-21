43c1e2b3c64eb2bfee2d80f45fc8d71b145e8ff6
Constanza Mariño <marinoconstanza03@gmail.com>
2025-07-21 16:30:44 -0300

    Have to add code into the last two methods

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -1,11 +1,12 @@
 using System;
-using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
 
 
 public class Scripture
 {
 
-    private Reference _reference;
+    public Reference _reference;
 
     private List<Word> _words;
 
@@ -13,13 +14,24 @@ public class Scripture
 
     public Scripture(Reference reference, string text)
     {
-      
+        _reference = reference;
+        _words = new List<Word>();
 
+        string[] _splitWord = text.Split(' ');
 
+        foreach (string word in _splitWord)
+        {
+            Word newWord = new Word(word);
+            _words.Add(newWord);
+        }
     }
 
-    public void HideRandomWords(int _numberToHide)
+    public Word HideRandomWords(int _numberToHide)
     {
+        Random randomNumber = new Random();
+        int index = randomNumber.Next(_words.Count);
+        return _words[index];
+
 
     }
 
