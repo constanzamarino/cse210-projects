799c4224d350c43a92511dfb4c3019907b5bc4f2
Constanza Mariño <marinoconstanza03@gmail.com>
2025-07-21 16:26:24 -0300

    Logic fixed

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -3,24 +3,33 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
     {
+        Reference reference = new Reference("John", 3, 16, 17);
+        Scripture scripture = new Scripture(reference, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.For God sent not his Son into the world to condemn the world; but that the world through him might be saved.");
+
         Console.WriteLine("Welcome to the ScriptureMemorizer program!");
-        Console.WriteLine("Type 'enter' to continue. Type 'quit' after you are done.");
-        string user_choice = Console.ReadLine();
 
 
-
-        while (user_choice != "quit".ToLower())
+        while (scripture.CompletelyHidden())
         {
-            if (user_choice == "enter")
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("Type 'enter' to continue. Type 'quit' after you are done.");
+            string user_choice = Console.ReadLine();
+
+            if (user_choice == "enter".ToLower())
             {
-                
+
             }
 
-            else if (user_choice == "quit")
+            else if (user_choice == "quit".ToLower())
             {
-                Console.WriteLine();
+                break;
             }
+
+            scripture.HideRandomWords(2);
         }
+        
     }
 }
