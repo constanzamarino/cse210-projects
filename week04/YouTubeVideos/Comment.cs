63a8e0e3692fff1691eb025841dc156e2b1593dd
Constanza Mariño <marinoconstanza03@gmail.com>
2025-07-29 20:36:38 -0300

    Starting project

---
+++ week04/YouTubeVideos/Comment.cs
@@ -0,0 +1,15 @@
+using System;
+
+
+public class Comment
+{
+    public string _userName;
+
+    public string _commentText;
+
+    public void DisplayAllComments()
+    {
+        
+    }
+
+}
---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class Video
+{
+    public string _videoTitle;
+
+    public string _videoAuthor;
+
+    public float _videoLength;
+
+    List<Comment> _comments = new Comment();
+
+    public void DisplayAll()
+    {
+
+    }
+
+    public void StoreVideoCommetns()
+    {
+
+    }
+
+    public GetNumberComments()
+    {
+
+    }
+    
+}
