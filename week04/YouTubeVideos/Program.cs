62bfef6b597e1969bc91b6a1b8114f89ba3c5e08
Constanza Mariño <marinoconstanza03@gmail.com>
2025-07-29 22:32:28 -0300

    Objects added + comments on what I have to do

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -11,22 +11,23 @@ class Program
         video1._comments.Add(new Comment { _userName = "", _commentText = "" });
 
 
-        Video video2 = new Video { };
-        video2._comments.Add(new Comment { });
-        video2._comments.Add(new Comment { });
-        video2._comments.Add(new Comment { });
+        Video video2 = new Video { _videoLength = 0, _videoTitle = "", _videoAuthor = "" };
+        video2._comments.Add(new Comment {_userName = "", _commentText = "" });
+        video2._comments.Add(new Comment {_userName = "", _commentText = "" });
+        video2._comments.Add(new Comment {_userName = "", _commentText = "" });
 
-        Video video3 = new Video { };
-        video3._comments.Add(new Comment { });
-        video3._comments.Add(new Comment { });
-        video3._comments.Add(new Comment { });
+        Video video3 = new Video { _videoLength = 0, _videoTitle = "", _videoAuthor = "" };
+        video3._comments.Add(new Comment {_userName = "", _commentText = "" });
+        video3._comments.Add(new Comment {_userName = "", _commentText = ""});
+        video3._comments.Add(new Comment {_userName = "", _commentText = "" });
 
-        Video video4 = new Video { };
-        video4._comments.Add(new Comment { });
-        video4._comments.Add(new Comment { });
-        video4._comments.Add(new Comment { });
+        Video video4 = new Video {_videoLength = 0, _videoTitle = "", _videoAuthor = "" };
+        video4._comments.Add(new Comment {_userName = "", _commentText = "" });
+        video4._comments.Add(new Comment {_userName = "", _commentText = "" });
+        video4._comments.Add(new Comment {_userName = "", _commentText = ""});
 
         // I have to connect the methods with the objects through dots somehow.
+        // Add info into each attribute in the objects.
 
 
 
