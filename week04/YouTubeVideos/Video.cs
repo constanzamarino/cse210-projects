050d2b220ca4a902b9a6a779ab4c4148e8077678
Constanza Mariño <marinoconstanza03@gmail.com>
2025-07-30 20:34:44 -0300

    Logic fixed

--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 public class Video
 {
@@ -10,37 +11,45 @@ public class Video
     public float _videoLength;
 
     public List<Comment> _comments = new List<Comment>();
-
     public void StoreVideoComments()
     {
         string filename = "stored_comments.txt";
-        using (StreamWriter file = new StreamWriter(filename))
+        using (StreamWriter file = new StreamWriter(filename, append : true))
         {
+
+            file.WriteLine($"{_videoTitle}");
+            file.WriteLine($"{_videoAuthor}");
+            file.WriteLine($"{_videoLength}");
+            file.WriteLine($"Comments");
             foreach (Comment comment in _comments)
             {
-                file.WriteLine($"{_videoLength}\n{_videoTitle}\nBy:{_videoAuthor}\n{comment}");
+                file.WriteLine($"{comment._userName}: {comment._commentText}");
             }
+
+            file.WriteLine();
         }
     }
 
+
     public void GetNumberComments()
     {
-        foreach (Comment comment in _comments)
-        {
-            int _numberComments = _comments.Count;
-            Console.WriteLine($" Comments: {_numberComments}");
-        }
+        int numberComments = _comments.Count;
+        Console.WriteLine($"{numberComments} comments:");
+        
     }
 
     public void DisplayAll()
     {
-        
+        Console.WriteLine($"{_videoTitle}");
+        Console.WriteLine($"{_videoAuthor}");
+        Console.WriteLine($"{_videoLength} minutes");
+       
+
         foreach (Comment comment in _comments)
         {
-            Console.WriteLine($"{_videoLength}\n{_videoTitle}\nBy:{_videoAuthor}\n{comment}");
+            comment.DisplayAllComments();
         }
-        
-    }
 
+    }
 
 }
