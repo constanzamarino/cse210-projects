a11e72cc460af4cad5358c878bac42dae64ebc41
Constanza Mariño <marinoconstanza03@gmail.com>
2025-07-31 14:16:53 -0300

    Changes : code added + LivesInUSA() --> IsFromUSA()

---
+++ week04/OnlineOrdering/Customer.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Customer
+{
+    private string _customerName;
+
+    private Address _customerAddress;
+
+    public Customer(string name, Address address)
+    {
+        _customerName = name;
+        _customerAddress = address;
+    }
+
+    public bool LivesInUSA()
+    {
+        return _customerAddress.IsFromUSA();
+    }
+
+
+}
