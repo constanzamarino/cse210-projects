daf9c656267896b4923577b54d8d05cba86b9a8c
Constanza Mariño <marinoconstanza03@gmail.com>
2025-08-01 16:35:06 -0300

    Added code in GetPackingLabel() and GetShippingLabel()

--- week04/OnlineOrdering/Order.cs
+++ week04/OnlineOrdering/Order.cs
@@ -2,21 +2,22 @@ using System;
 using System.Collections.Generic;
 
 
+
 public class Order
 {
     private List<Product> _products;
-    private Customer _costumer;
+    private Customer _customer;
 
-    public Order(List<Product> products, Customer costumer)
+    public Order(List<Product> products, Customer customer)
     {
         _products = products;
-        _costumer = costumer;
+        _customer = customer;
     }
 
     public float CalculateTotalCost()
     {
         float total = 0;
-        
+
         foreach (Product product in _products)
         {
             total += product.GetTotalCost();
@@ -27,13 +28,28 @@ public class Order
 
     public string GetPackingLabel()
     {
-        
+        string packinglabel = "";
+
+        foreach (Product product in _products)
+        {
+            packinglabel += $"{product.GetProductName()}\n";
+            packinglabel += $"{product.GetProductID()}\n";
+        }
+        return packinglabel;
     }
 
     public string GetShippingLabel()
     {
-        
+        string shippinglabel = "";
+
+        shippinglabel += $"{_customer.GetCustomerName()}\n";
+        shippinglabel += $"{_customer.GetCustomerAddress().GetDisplayAll()}\n";
+
+        return shippinglabel;
+
+
     }
+    
 
   
 }
