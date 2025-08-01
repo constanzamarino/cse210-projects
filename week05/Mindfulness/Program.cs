3a84e2e50d5332ce83e3d0f73e5392c2707b9be0
Constanza Mariño <marinoconstanza03@gmail.com>
2025-08-01 16:40:08 -0300

    Testing Thread.Sleep()

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Threading.Channels;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        Console.WriteLine("Presenting the project in: ");
+
+        for (int i = 10; i > 0; i--)
+        {
+            Console.WriteLine(i);
+            Thread.Sleep(900);
+        }
+
+        Console.WriteLine("BOOOOOOOM");
+        Thread.Sleep(700);
+        Console.WriteLine("Welcome to the Mindfulness Program");
     }
 }
+
+    
+
+    
+
+
+
