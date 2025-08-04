076c39fa9a1a493aaeb6d0d4b2465bcae23105b9
Constanza Mariño <marinoconstanza03@gmail.com>
2025-08-04 11:13:42 -0300

    Main basic program structure done

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -5,17 +5,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Presenting the project in: ");
+        Console.WriteLine("Welcome to the Mindfulness Program!");
 
-        for (int i = 10; i > 0; i--)
+        int user_choice = 0;
+
+        while (user_choice != 4)
+
         {
-            Console.WriteLine(i);
-            Thread.Sleep(900);
-        }
+            Console.WriteLine("Menu:");
+            Console.WriteLine("\n1.Breathing Activity\n2.Reflection Activity\n3.Listing Activity\n4.Quit\n");
+            Console.WriteLine("Please, select an option from the menu by entering its corresponding number:");
 
-        Console.WriteLine("BOOOOOOOM");
-        Thread.Sleep(700);
-        Console.WriteLine("Welcome to the Mindfulness Program");
+            string user_input = Console.ReadLine();
+            if (int.TryParse(user_input, out user_choice))
+            {
+                Console.WriteLine("Invalid input. Please, select an option from the menu by entering its corresponding number: ");
+            }
+
+            if (user_choice == 1)
+            {
+
+            }
+
+            else if (user_choice == 2)
+            {
+
+            }
+
+            else if (user_choice == 3)
+            {
+
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid number. Please, select an option from the menu by entering its corresponding number (1-4): ");
+            }
+
+
+        }
+        
+
+
+       
     }
 }
 
