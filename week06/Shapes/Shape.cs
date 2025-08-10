5de97d37285997e31f93f8ca06b25440cbc44daf
Constanza Mariño <marinoconstanza03@gmail.com>
2025-08-10 19:01:40 -0300

    Starting Shapes Program homework

---
+++ week06/Shapes/Shape.cs
@@ -0,0 +1,21 @@
+using System; 
+
+public class Shape
+{
+    private string _color;
+}
+
+public string GetColor()
+{
+    return _color;
+}
+
+public void SetColor()
+{
+
+}
+
+public virtual double GetArea()
+{
+
+}
