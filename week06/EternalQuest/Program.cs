a1214358a2117f03a4d8e44e35f6a5211e0e3188
Constanza Mariño <marinoconstanza03@gmail.com>
2025-08-14 11:30:29 -0300

    Style and structure fixed

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -4,44 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        
-        
-        Console.WriteLine("Welcome to the Eternal Quest Program!");
-        Console.WriteLine("Select an option from the following menu:");
-        string user_input = Console.ReadLine();
-        int user_choice = int.Parse(user_input);
+        ManageGoals goalManager = new ManageGoals("", "", 0, 0);
+        int user_choice = 0;
+
+        Console.WriteLine("Welcome to the Eternal Quest Program!\n");
 
         while (user_choice != 6)
         {
+            goalManager.DisplayPlayerData();
+
             Console.WriteLine("Menu:\n1.Add a new goal\n2.List a goal\n3.Save your goals\n4.Load your goals\n5.Record an event\n6.Quit\n");
+            Console.Write("\nSelect an option from the menu:");
+            user_choice = int.Parse(Console.ReadLine());
+
             if (user_choice == 1)
             {
-                //Create objects + call methods
+                goalManager.AddGoal();
             }
 
             else if (user_choice == 2)
             {
-                //Create objects + call methods
+                goalManager.ListGoals();
             }
 
             else if (user_choice == 3)
             {
-                //Create objects + call methods
+                goalManager.SaveGoal();
             }
 
             else if (user_choice == 4)
             {
-                //Create objects + call methods
+                goalManager.LoadGoal();
             }
 
             else if (user_choice == 5)
             {
-                //Create objects + call methods
+                goalManager.RecordEvent();
+            }
+
+            else if (user_choice == 6)
+            {
+                Console.WriteLine("Thanks for using the Eternal Quest Program! Goodbye!");
             }
 
             else
             {
-
+                Console.WriteLine("Invalid input");
             }
         }
 
